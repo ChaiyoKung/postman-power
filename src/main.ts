1c021a44a74ab164dcf4d0e724420932a9dc68ad
ChaiyoKung <kananek45@gmail.com>
2024-01-17 23:59:43 +0700

    Support json request body

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (_req: Request, res: Response) => {
   return res.send('Postman Power!');
 });
