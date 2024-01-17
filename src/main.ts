f84b5fba1e2936eaea66d242449cf1eaf1c927fb
ChaiyoKung <kananek45@gmail.com>
2024-01-17 23:34:55 +0700

    Initialized express.js

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_req: Request, res: Response) => {
+  return res.send('Postman Power!');
+});
+
+app.listen(port, () => {
+  console.log(`Application is listening on port ${port}`);
+});
